c85ea9e978b699287401808981ccd47795ec55d9
xkrtjddn <tjddn5629@gmail.com>
2015-10-07 10:40:45 +0900

    작업 시작

---
+++ app/src/main/java/com/humanix/tsw/bluenix/BluetoothLeService.java
@@ -0,0 +1,41 @@
+package com.humanix.tsw.bluenix;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BluetoothLeService extends Service {
+    private final static String TAG = BluetoothLeService.class.getSimpleName();
+
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    private String mBluetoothDeviceAddress;
+    private BluetoothGatt mBluetoothGatt;
+
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+
+    public final static String ACTION_GATT_CONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+
+    public BluetoothLeService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ app/src/main/java/com/humanix/tsw/bluenix/SampleGattAttributes.java
@@ -0,0 +1,30 @@
+package com.humanix.tsw.bluenix;
+
+import java.util.HashMap;
+
+/**
+ * Created by Administrator on 2015-10-06.
+ */
+public class SampleGattAttributes {
+    private static HashMap<String, String> attributes = new HashMap();
+    public static String BLUEINNO_PROFILE_SERVICE = "BLUEINNO_PROFILE_SERVICE";
+    public static String UUID_BLUEINNO_PROFILE_SERVICE_UUID = "00002220-0000-1000-8000-00805f9b34fb";
+    public static String BLUEINNO_PROFILE_SEND = "BLUEINNO_PROFILE_SEND";
+    public static String UUID_BLUEINNO_PROFILE_SEND_UUID = "00002222-0000-1000-8000-00805f9b34fb";
+    public static String BLUEINNO_PROFILE_RECEIVE = "BLUEINNO_PROFILE_RECEIVE";
+    public static String UUID_BLUEINNO_PROFILE_RECEIVE_UUIDD = "00002221-0000-1000-8000-00805f9b34fb";
+
+    static {
+        attributes.put(UUID_BLUEINNO_PROFILE_SERVICE_UUID, BLUEINNO_PROFILE_SERVICE);
+        attributes.put(UUID_BLUEINNO_PROFILE_SEND_UUID, BLUEINNO_PROFILE_SEND);
+        attributes.put(UUID_BLUEINNO_PROFILE_RECEIVE_UUIDD, BLUEINNO_PROFILE_RECEIVE);
+    }
+
+    public SampleGattAttributes() {
+    }
+
+    public static String lookup(String uuid, String defaultName) {
+        String name = (String)attributes.get(uuid);
+        return name == null?defaultName:name;
+    }
+}
